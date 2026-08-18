dbc98b1e84f0a360ddff0c6b8606663b94db1f55
Krishna-Gupta17 <2024021228@mmmut.ac.in>
2026-08-18 19:22:16 +0530

    cors fixed

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ let allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
+  'https://api.skumarinfracons.com'
 ];
 
 if (process.env.ALLOWED_ORIGINS) {
